3bd73d1ecf2729966c417a297e10b9c7885b93f5
iyanuoluwa Oyolola <iyanuoyolola@gmail.com>
2024-11-20 01:03:28 +0100

    Remove db folder from tracking

--- src/db/main.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel
-from sqlalchemy.ext.asyncio import AsyncSession
-from src.config import config
-
-engine = AsyncEngine(
-    create_engine(
-url=config.DATABASE_URL,
-    echo=True,
-))
